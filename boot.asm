3acf57bbfea28dbb0c1d0364b2331c64c3288229
UnderscoreGavin <gavincbie@gmail.com>
2023-03-07 20:55:16 -0500

    Update Commit

--- boot.asm
+++ boot.asm
@@ -1,3 +1,22 @@
-jmp $ 
+[org 0x7c00]
+mov ah, 0x0e
+mov bp, 0x8000 ; base pointer
+mov sp, bp ; stack pointer
+push 'T'
+push 'S'
+push 'E'
+push 'T'
+
+run:
+    pop bx
+    mov al, bl
+    cmp al, 0
+    je done
+    int 10h
+    jmp run
+done:
+jmp $
+cli
+hlt 
 times 510 - ($-$$) db 0
 dw 0xaa55 
